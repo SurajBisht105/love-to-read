b65625c060eee0033f3e18743cff38f3076a9434
suraj <111647847+SurajBisht105@users.noreply.github.com>
2025-04-10 15:29:43 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FlipBook from "../components/FliBBook";
+import FlipBook from "../components/FlipBook";
 function Home() {
   return (
     <div className="w-full max-w-5xl mx-auto">
